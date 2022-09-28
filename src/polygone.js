27f5730832d746cb03177d9e1667b63bfe764c8f
AlexMurmelstein <armurm@gmail.com>
2022-09-28 11:49:07 +0300

    expanding polygone

--- src/polygone.js
+++ src/polygone.js
@@ -1,6 +1,22 @@
+"use strict";
 //Seems as the most interesting API
 //Check docs here:
 //https://polygon.io/docs
-console.log("hello typescript!");
-var num = 11;
-console.log("num: ".concat(num));
+exports.__esModule = true;
+require("dotenv/config");
+var POLYGONE = process.env.POLYGONE;
+//Now to actually query the API
+var ticker1 = "AAPL";
+var APIstring = "https://api.polygon.io/v1/open-close/".concat(ticker1, "/2020-10-14?adjusted=true&apiKey=").concat(POLYGONE);
+var axios_1 = require("axios");
+var options = {
+    method: "GET",
+    url: APIstring
+};
+axios_1["default"]
+    .request(options)
+    .then(function (response) {
+    console.log(response.data);
+})["catch"](function (error) {
+    console.error(error);
+});
